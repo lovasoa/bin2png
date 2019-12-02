eedc19ca76f90a04f54a433898168263c9b5f146
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-12-02 18:28:00 +0100

    Improve png2bin

--- png2bin/png2bin.js
+++ png2bin/png2bin.js
@@ -1,5 +1,12 @@
+/**
+ * Decodes an image encoded with png2bin
+ * 
+ * @param {HTMLImageElement} img An image that has been encoded with png2bin
+ * @returns {Uint8Array} decoded file
+ */
 async function png2bin(img) {
     var canvas = document.createElement("canvas");
+    var img = await imgLoad(img);
     canvas.width = img.width;
     canvas.height = img.height;
     var ctx = canvas.getContext("2d");
@@ -13,6 +20,14 @@ async function png2bin(img) {
     var surplus = new DataView(data.buffer, data.length - 4, 4).getUint32(0, true);
     var length = (data.length / 4 | 0) * 3 - surplus;
     return data.slice(0, length);
+}
+
+function imgLoad(img) {
+    return new Promise(function (accept, reject) {
+        if (img.complete) accept(img);
+        img.onerror = reject;
+        img.onload = accept.bind(null, img);
+    });
 }
 
 if (typeof module !== "undefined") module.exports = { png2bin }
